8e8d55329d6000088a70045822b474f625c74369
huutvpn <143250398+huutvpn@users.noreply.github.com>
2024-02-17 02:16:31 +0700

    Add files via upload

--- files/limit.sh
+++ files/limit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-REPO="https://https://raw.githubusercontent.com/huutvpn/ghoibvpn/GM/"
+REPO="https://https://raw.githubusercontent.com/huutvpn/badbudvpnstore/GM/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
